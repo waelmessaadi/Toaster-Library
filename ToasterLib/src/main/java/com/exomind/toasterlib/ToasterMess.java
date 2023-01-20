942e15b122646a13ec8ce3568bdc764560bcd543
MESSAADI-Wael <waelmessaadi16@gmail.com>
2023-01-20 11:18:38 +0100

    make method show message static

--- ToasterLib/src/main/java/com/exomind/toasterlib/ToasterMess.java
+++ ToasterLib/src/main/java/com/exomind/toasterlib/ToasterMess.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 public class ToasterMess {
 
-    public void s(Context c, String s) {
+    public static void s(Context c, String s) {
         Toast.makeText(c, s, Toast.LENGTH_SHORT).show();
     }
 }
